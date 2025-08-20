2828c72d5861b6427d92b7822960352f81a5bbf4
T2X2-zhuk <oksana_sineok@mail.ru>
2025-08-20 14:51:36 +0300

    ElectronicSchoolProgram

--- School/SchoolStudent/src/main/java/SchoolStudent/core/web/GetAllStudentsBySchoolClassWeb.java
+++ School/SchoolStudent/src/main/java/SchoolStudent/core/web/GetAllStudentsBySchoolClassWeb.java
@@ -9,6 +9,7 @@ import SchoolStudent.core.service.DeleteStudentFromDatabaseService;
 import SchoolStudent.core.service.GetAllStudentsBySchoolClassService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -25,14 +26,14 @@ public class GetAllStudentsBySchoolClassWeb {
     @Autowired
     private GetAllStudentsBySchoolClassService service;
 
-    @GetMapping("get-all-students-by-school-class")
+    @GetMapping("/get-all-students-by-school-class")
     public String showForm(ModelMap modelMap) {
         modelMap.addAttribute("request", new GetAllStudentsBySchoolClassRequest());
         modelMap.addAttribute("response", new GetAllStudentsBySchoolClassResponse());
         return "get-all-students-by-school-class";
     }
 
-    @PostMapping("get-all-students-by-school-class")
+    @PostMapping("/get-all-students-by-school-class")
     public String processForm(@ModelAttribute(value = "request") GetAllStudentsBySchoolClassRequest request,
                               ModelMap modelMap) {
         GetAllStudentsBySchoolClassResponse response = service.execute(request);
